68cd7f3cbaf20bc056f9a1b4afb6d72c2ee3a8cb
Zhenya <zhenya-bogdasheva@ya.ru>
2022-06-01 20:06:21 +0300

    Add models and admin

--- ProductPlatform/mainapp/admin.py
+++ ProductPlatform/mainapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Orders, Category
 
-# Register your models here.
+
+class AdminOrders(admin.ModelAdmin):
+    list_display = ['author', 'category', 'name', 'status', ]
+    list_filter = ['category', 'status']
+    search_fields = ['category', 'name', 'author']
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+admin.site.register(Orders, AdminOrders)
+admin.site.register(Category, AdminCategory)
+

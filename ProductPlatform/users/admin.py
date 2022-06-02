01ae65aef4a849809e155a9fd8558205a74b9709
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-06-02 13:11:52 +0300

    add model - Profile add models(usersApp) in Admin

--- ProductPlatform/users/admin.py
+++ ProductPlatform/users/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Profile
 
-
-class UserProfile(admin.ModelAdmin):
-    list_display = ['comp_name', 'phone_number', 'email']
-    list_filter = ['role']
-    search_fields = ['comp_name']
-
-
-admin.site.register(Profile, UserProfile)
+# Register your models here.

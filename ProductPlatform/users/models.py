0ee49dcf856b88ce7119782c374c5ba8698656d4
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-06-02 13:11:52 +0300

    add model - Profile add models(usersApp) in Admin

--- ProductPlatform/users/models.py
+++ ProductPlatform/users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(AbstractUser):
+    company_choice = [
+        ('Customer', 'Customer'),
+        ('Supplier', 'Supplier')
+    ]
+    phone_number = PhoneNumberField(verbose_name='Номер телефона', unique=True)
+    email = models.EmailField(max_length=120, verbose_name='Элеектронная почта', unique=True)
+    city = models.CharField(max_length=120, verbose_name='Город')
+    ogrn = models.CharField(max_length=15, verbose_name='ОГРН')  # В шаблоне добавить подсказку количество цифр!
+    comp_name = models.CharField(max_length=120, verbose_name='Название компании')
+    role = models.CharField(choices=company_choice, max_length=120, verbose_name='Роль')
+    bio = models.TextField(verbose_name='Описание', blank=True, )
+
+
+class Meta:
+    verbose_name = 'Профиль'
+    verbose_name_plural = 'Профили'
+
+
+def __str__(self):
+    return f'Название компании: {self.comp_name}'

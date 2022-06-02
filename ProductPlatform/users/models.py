102b1ad11306a85e82f031a7468c645bb34fffa7
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-06-02 13:11:52 +0300

    add model - Profile add models(usersApp) in Admin

--- ProductPlatform/users/models.py
+++ ProductPlatform/users/models.py
@@ -3,8 +3,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
 
-# Create your models here.
-
 class Profile(AbstractUser):
     company_choice = [
         ('Customer', 'Customer'),
@@ -13,7 +11,7 @@ class Profile(AbstractUser):
     phone_number = PhoneNumberField(verbose_name='Номер телефона', unique=True)
     email = models.EmailField(max_length=120, verbose_name='Элеектронная почта', unique=True)
     city = models.CharField(max_length=120, verbose_name='Город')
-    ogrn = models.CharField(max_length=15, verbose_name='ОГРН') #В шаблоне добавить подсказку количество цифр!
+    ogrn = models.CharField(max_length=15, verbose_name='ОГРН') # В шаблоне добавить подсказку количество цифр!
     comp_name = models.CharField(max_length=120, verbose_name='Название компании')
     role = models.CharField(choices=company_choice, max_length=120, verbose_name='Роль')
     bio = models.TextField(verbose_name='Описание', blank=True,)

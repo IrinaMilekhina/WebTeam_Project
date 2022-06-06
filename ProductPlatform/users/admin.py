4303849c56ad37800b75c32ec764db16418c40cc
Vyacheslav <lvv_1@list.ru>
2022-06-06 23:38:41 +0300

    change_users_admin

--- ProductPlatform/users/admin.py
+++ ProductPlatform/users/admin.py
@@ -6,6 +6,7 @@ from .models import Profile
 
 class UserProfile(admin.ModelAdmin):
     list_display = ['username', 'comp_name', 'phone_number', 'email']
+    list_display_links = ['username']
     list_filter = ['role']
     search_fields = ['comp_name']
 

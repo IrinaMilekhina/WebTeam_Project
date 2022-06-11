60a3235f809d113f1a2d04e3bdb97cad97d68a7f
DanielPivnev <d.pivnev2003@gmail.com>
2022-06-11 18:00:34 +0200

    edit base template

---
+++ ProductPlatform/static/assets/js/navigation.js
@@ -0,0 +1,42 @@
+//according to loftblog tut
+var showSection = function showSection(section, isAnimate) {
+    var
+        direction = section.replace(/#/, ''),
+        reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+        reqSectionPos = reqSection.offset().top - 0;
+
+    if (isAnimate) {
+        $('body, html').animate({
+                scrollTop: reqSectionPos
+            },
+            800);
+    } else {
+        $('body, html').scrollTop(reqSectionPos);
+    }
+
+};
+
+var checkSection = function checkSection() {
+    $('.section').each(function () {
+        var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+        if (topEdge < wScroll && bottomEdge > wScroll) {
+            var
+                currentId = $this.data('section'),
+                reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+            reqLink.closest('li').addClass('active').siblings().removeClass('active');
+        }
+    });
+};
+
+$('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
+    e.preventDefault();
+    showSection($(this).attr('href'), true);
+});
+
+$(window).scroll(function () {
+    checkSection();
+});

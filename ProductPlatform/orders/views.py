8edbdbea87a6384dd454395b7ebce2af0f54268a
Vyacheslav <lvv_1@list.ru>
2022-06-12 22:50:00 +0300

    add_paginaton

--- ProductPlatform/orders/views.py
+++ ProductPlatform/orders/views.py
@@ -10,3 +10,4 @@ class CategoryOrderView(ListView):
     queryset = CategoryOrder.objects.filter(is_active=True)
     context_object_name = 'all_categories'
     template_name = 'orders/categories.html'
+    paginate_by = 2  # заменить на нужное количество

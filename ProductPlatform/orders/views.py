910752cbf207ea122b8f1b7036c97ca4a7a2b465
im <i.mezhova@gmail.com>
2022-06-13 22:27:28 +0300

    url and view for main page

--- ProductPlatform/orders/views.py
+++ ProductPlatform/orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class MainView(View):
+    template_name = 'users/base.html'
+    title = 'Главная'
+
+    def get(self, request, *args, **kwargs):
+        content = {
+            'title': self.title,
+        }
+
+        return render(request, self.template_name, content)

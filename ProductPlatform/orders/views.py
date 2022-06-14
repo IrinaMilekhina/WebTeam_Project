e6b27c23d5c58ed3e68f74a069c569aec1671b09
ivg97 <gavr97.97@mail.ru>
2022-06-14 20:50:05 +0200

    добавлена вью для категориЯ

--- ProductPlatform/orders/views.py
+++ ProductPlatform/orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from orders.models import CategoryOrder
 from django.views import View
 
@@ -22,3 +23,25 @@ class CategoryOrderView(ListView):
     context_object_name = 'all_categories'
     template_name = 'orders/categories.html'
     paginate_by = 5
+
+
+class Category(DetailView):
+    """Класс-обработчик для отображения выбранной категории"""
+    model = CategoryOrder
+    template_name = 'orders/category.html'
+
+    def get(self, request, *args, **kwargs):
+        """Если приходит GET запрос, получаем категорию по id и рендерим шаблон orders/category.html"""
+        try:
+            id = kwargs.get('id', None)
+        except KeyError as err:
+            print(err)  # для DEBAG = True
+            return render(request, self.template_name, {'ERROR': 'Страница не найдена'})
+        try:
+            category = get_object_or_404(CategoryOrder, id=id)
+        except Http404 as err:
+            print(err)  # для DEBAG = True
+            return render(request, self.template_name, {'ERROR': 'Страница не найдена'})
+        return render(request, self.template_name, {'category': category})
+
+

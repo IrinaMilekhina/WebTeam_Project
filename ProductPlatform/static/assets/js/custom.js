2d8d64aeb782c33e33c1f806a4b4c1df222cec0b
DanielPivnev <d.pivnev2003@gmail.com>
2022-06-18 13:22:25 +0200

    menu bug fixed

--- ProductPlatform/static/assets/js/custom.js
+++ ProductPlatform/static/assets/js/custom.js
@@ -2,15 +2,25 @@
 	
 	"use strict";
 
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop();
-	  var box = $('.header-text').height();
-	  var header = $('header').height();
+	// $(window).scroll(function() {
+	//   var scroll = $(window).scrollTop();
+	//   var box = $('.header-text').height();
+	//   var header = $('header').height();
+	//
+	//   if (scroll >= box - header) {
+	//     $("header").addClass("background-header");
+	//   } else {
+	//     $("header").removeClass("background-header");
+	//   }
+	// });
 
-	  if (scroll >= box - header) {
-	    $("header").addClass("background-header");
+	$(window).scroll(function() {
+	  let scroll = $(window).scrollTop();
+
+	  if (scroll < 41) {
+		  $("header").css("margin-top", `${41 - scroll}px`);
 	  } else {
-	    $("header").removeClass("background-header");
+		  $("header").css("margin-top", "0");
 	  }
 	});
 	

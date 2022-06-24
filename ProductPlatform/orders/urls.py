723f28e66ff658a1e27b62524f342584b396e53d
Vyacheslav <lvv_1@list.ru>
2022-06-25 00:37:54 +0300

    in work

--- ProductPlatform/orders/urls.py
+++ ProductPlatform/orders/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import CategoryOrderView, Category, CreateOrder, OrderView, OrderBoardView, HomeView
-
+from .views import CategoryOrderView, Category, CreateOrder, OrderView, HomeView
 
 app_name = 'orders'
 

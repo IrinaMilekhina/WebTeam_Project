56f47cfb5706d73a0d192a90f8273cae93e17c66
Bekham <anton-bekker@yandex.ru>
2022-06-25 22:23:41 +0600

    apply new models Order, ResponseOrder, StatusResponse

--- ProductPlatform/users/views.py
+++ ProductPlatform/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.views.generic.edit import FormView
 
-from orders.models import ResponseOrder, Order
+from orders.models import ResponseOrder, Order, StatusResponse
 from users.forms import UserLoginForm, UserRegisterForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, UpdateView
@@ -133,28 +133,31 @@ class PersonalHistoryOrdersView(ListView):
         context = super(PersonalHistoryOrdersView,
                         self).get_context_data(**kwargs)
         current_profile = Profile.objects.get(pk=self.request.user.pk)
-        responses, orders = None, None
+        responses, orders, status_responses = None, None, None
         if current_profile.role == 'Customer':
-            responses = ResponseOrder.objects.filter(order__author=self.request.user.pk, order__status__in=['Done', 'Not Active'])
-            orders = Order.objects.filter(author=self.request.user.pk, status__in=['Done', 'Not Active'])
+            responses = ResponseOrder.objects.filter(order__author=self.request.user.pk, order__status='Not Active')
+            orders = Order.objects.filter(author=self.request.user.pk, status='Not Active')
+            status_responses = StatusResponse.objects.filter(response_order__order__author=self.request.user.pk,
+                                                             response_order__order__status='Not Active')
         elif current_profile.role == 'Supplier':
-            orders = Order.objects.filter(responseorder__response_user=self.request.user.pk, status__in=['Done', 'Not Active'])
-            responses = ResponseOrder.objects.filter(order__id__in=orders.values_list('id'), order__status__in=['Done', 'Not Active'])
+            orders = Order.objects.filter(responseorder__response_user=self.request.user.pk, status='Not Active')
+            responses = ResponseOrder.objects.filter(order__id__in=orders.values_list('id'), order__status='Not Active')
+            status_responses = StatusResponse.objects.filter(response_order__order__id__in=orders.values_list('id'),
+                                                             response_order__order__status='Not Active')
         history_orders = []
         for item in orders:
             response_count = responses.filter(order=item.id).count()
-            if item.status == 'Done':
-                status = 'Выполнен'
-            else:
+            approved_response_user_id = None
+            try:
+                approved_response = status_responses.get(status='Approved', response_order=item.id)
+                if current_profile.role == 'Supplier' \
+                        and approved_response.response_order.response_user.id == current_profile.id:
+                    status = 'Ваш отклик утвержден'
+                else:
+                    status = 'Поставщик утвержден'
+                approved_response_user_id = approved_response.response_order
+            except Exception:
                 status = 'Отменен'
-            response_approved = None
-            for response in responses.filter(order=item.id):
-                if 'Approved' == response.status:
-                    if current_profile.role == 'Supplier' and response.response_user.id == current_profile.id:
-                        status = 'Ваш отклик утвержден'
-                    else:
-                        status = 'Заказчик утвержден'
-                    response_approved = response
             history_orders.append({
                 'name': item.name,
                 'author': item.author,
@@ -165,56 +168,56 @@ class PersonalHistoryOrdersView(ListView):
                 'date_to': item.end_time.date(),
                 'response_count': response_count,
                 'status': status,
-                'response_approved': response_approved
+                'response_approved': approved_response_user_id
             })
         context['orders'] = history_orders
         context['user'] = current_profile
         return context
 
 
-class PersonalHistoryOrdersView(ListView):
-    model = Order
-    template_name = 'users/account_history_orders.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        """Метод для создания необходимого контекста для истории заказов личного кабинета"""
-        context = super(PersonalHistoryOrdersView,
-                        self).get_context_data(**kwargs)
-        current_profile = Profile.objects.get(pk=self.request.user.pk)
-        responses, orders = None, None
-        if current_profile.role == 'Customer':
-            responses = ResponseOrder.objects.filter(order__author=self.request.user.pk, order__status__in=['Done', 'Not Active'])
-            orders = Order.objects.filter(author=self.request.user.pk, status__in=['Done', 'Not Active'])
-        elif current_profile.role == 'Supplier':
-            orders = Order.objects.filter(responseorder__response_user=self.request.user.pk, status__in=['Done', 'Not Active'])
-            responses = ResponseOrder.objects.filter(order__id__in=orders.values_list('id'), order__status__in=['Done', 'Not Active'])
-        history_orders = []
-        for item in orders:
-            response_count = responses.filter(order=item.id).count()
-            if item.status == 'Done':
-                status = 'Выполнен'
-            else:
-                status = 'Отменен'
-            response_approved = None
-            for response in responses.filter(order=item.id):
-                if 'Approved' == response.status:
-                    if current_profile.role == 'Supplier' and response.response_user.id == current_profile.id:
-                        status = 'Ваш отклик утвержден'
-                    else:
-                        status = 'Заказчик утвержден'
-                    response_approved = response
-            history_orders.append({
-                'name': item.name,
-                'author': item.author,
-                'order_num': item.id,
-                'description': item.description,
-                'category': item.category.name,
-                'city': item.author.city,
-                'date_to': item.end_time.date(),
-                'response_count': response_count,
-                'status': status,
-                'response_approved': response_approved
-            })
-        context['orders'] = history_orders
-        context['user'] = current_profile
-        return context
+# class PersonalHistoryOrdersView(ListView):
+    # model = Order
+    # template_name = 'users/account_history_orders.html'
+    #
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     """Метод для создания необходимого контекста для истории заказов личного кабинета"""
+    #     context = super(PersonalHistoryOrdersView,
+    #                     self).get_context_data(**kwargs)
+    #     current_profile = Profile.objects.get(pk=self.request.user.pk)
+    #     responses, orders = None, None
+    #     if current_profile.role == 'Customer':
+    #         responses = ResponseOrder.objects.filter(order__author=self.request.user.pk, order__status__in=['Done', 'Not Active'])
+    #         orders = Order.objects.filter(author=self.request.user.pk, status__in=['Done', 'Not Active'])
+    #     elif current_profile.role == 'Supplier':
+    #         orders = Order.objects.filter(responseorder__response_user=self.request.user.pk, status__in=['Done', 'Not Active'])
+    #         responses = ResponseOrder.objects.filter(order__id__in=orders.values_list('id'), order__status__in=['Done', 'Not Active'])
+    #     history_orders = []
+    #     for item in orders:
+    #         response_count = responses.filter(order=item.id).count()
+    #         if item.status == 'Done':
+    #             status = 'Выполнен'
+    #         else:
+    #             status = 'Отменен'
+    #         response_approved = None
+    #         for response in responses.filter(order=item.id):
+    #             if 'Approved' == response.status:
+    #                 if current_profile.role == 'Supplier' and response.response_user.id == current_profile.id:
+    #                     status = 'Ваш отклик утвержден'
+    #                 else:
+    #                     status = 'Заказчик утвержден'
+    #                 response_approved = response
+    #         history_orders.append({
+    #             'name': item.name,
+    #             'author': item.author,
+    #             'order_num': item.id,
+    #             'description': item.description,
+    #             'category': item.category.name,
+    #             'city': item.author.city,
+    #             'date_to': item.end_time.date(),
+    #             'response_count': response_count,
+    #             'status': status,
+    #             'response_approved': response_approved
+    #         })
+    #     context['orders'] = history_orders
+    #     context['user'] = current_profile
+    #     return context

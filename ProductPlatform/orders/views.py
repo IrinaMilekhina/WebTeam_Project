1d0cbfc9262318b012b4d6303daa9d833fc513de
ivg97 <gavr97.97@mail.ru>
2022-06-28 18:34:56 +0300

    403 топ 5 поставщиков

--- ProductPlatform/orders/views.py
+++ ProductPlatform/orders/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from orders.forms import CreateOrderForm
 
-from orders.models import CategoryOrder, Order, StatusResponse
+from orders.models import CategoryOrder, Order, StatusResponse, ResponseOrder
 from django.views import View
 
 
@@ -65,12 +65,25 @@ class Category(DetailView):
         run_orders = [response_order.order for response_order in response_orders if response_order.order.status == 'Active' and response_order.order.category == category]
         all_completed_orders = len(run_orders)
         top_suppliers = [response_order.response_user for response_order in response_orders if response_order.order.status == 'Active' and response_order.order.category == category]
-        print(top_suppliers)
+
+        all_responses = ResponseOrder.objects.select_related().all()
+        active_responses = all_responses.filter(order__category=category)
+        unique_responses = {}
+        for i in active_responses:
+            if not i.statusresponse_set.last() is None and i.statusresponse_set.last().status == 'Approved':
+                if unique_responses.get(i.response_user):
+                    unique_responses[i.response_user] = unique_responses[i.response_user] + 1
+                else:
+                    unique_responses[i.response_user] = 1
+
+        # unique_responses = sorted(unique_responses.items(), key=lambda item: item[1])[::-5]
+
+
         return render(request, self.template_name, {'category': category,
                                                     'title': category.name,
                                                     'all_orders_amount': all_orders_amount,
                                                     'all_completed_orders': all_completed_orders,
-                                                    'top_suppliers': top_suppliers[:5]})
+                                                    'top_suppliers': unique_responses})
 
 
 class CreateOrder(CreateView):

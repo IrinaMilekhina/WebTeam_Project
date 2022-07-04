ebf8fdee10ced0bec4d175a1b352ba461d245896
Zhenya <zhenya-bogdasheva@ya.ru>
2022-07-04 20:34:45 +0300

    add declension of words

--- ProductPlatform/orders/templatetags/words_extras.py
+++ ProductPlatform/orders/templatetags/words_extras.py
@@ -10,8 +10,8 @@ def ru_plural(value: int, variants: str):
 
     if value % 10 == 1 and value % 100 != 11:
         variant = 0
-    elif value % 10 >= 2 and value % 10 <= 4 and \
-            (value % 100 < 10 or value % 100 >= 20):
+    elif value % 10 >= 2 and value % 10 <= 4 \
+            and (value % 100 < 10 or value % 100 >= 20):
         variant = 1
     else:
         variant = 2

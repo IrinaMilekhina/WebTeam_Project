02eaa2715a83eaa6d4e7037f459a967bb0c8708b
EuGeN-PYTHON <78923547+EuGeN-PYTHON@users.noreply.github.com>
2022-07-04 16:50:29 +0300

    feature/492_MODERATOR_ORDERBOARD

--- ProductPlatform/orders/urls.py
+++ ProductPlatform/orders/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import Category, CreateOrder, OrderView, table_order, DeleteCategory, categories
+from .views import Category, CreateOrder, OrderView, table_order, DeleteCategory, categories, DeleteOrder
 
 app_name = 'orders'
 
@@ -25,7 +25,8 @@ urlpatterns = [
     path('create_order/', CreateOrder.as_view(), name='create_order'),
     path('view_order/<int:pk>/', OrderView.as_view(), name='view_order'),
     path("table_order/", table_order, name="table_order"),
-    path('category/delete/<int:pk>/', DeleteCategory.as_view(), name='delete_category')
+    path('category/delete/<int:pk>/', DeleteCategory.as_view(), name='delete_category'),
+    path('order_delete/<int:pk>/', DeleteOrder.as_view(), name='delete_order')
 
 
 ]

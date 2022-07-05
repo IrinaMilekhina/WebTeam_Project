9ebe7d37f79bf831af307d551a4e311dd5b0bfa0
Bekham <anton-bekker@yandex.ru>
2022-07-05 22:23:20 +0600

    доработки.

--- ProductPlatform/users/views.py
+++ ProductPlatform/users/views.py
@@ -216,17 +216,6 @@ class ProfilePasswordResetView(PasswordResetView):
     def form_valid(self, form):
         user = Profile.objects.get(pk=self.request.user.pk)
         if user.email == form.cleaned_data['email']:
-            opts = {
-                "use_https": self.request.is_secure(),
-                "token_generator": self.token_generator,
-                "from_email": self.from_email,
-                "email_template_name": self.email_template_name,
-                "subject_template_name": self.subject_template_name,
-                "request": self.request,
-                "html_email_template_name": self.html_email_template_name,
-                "extra_email_context": self.extra_email_context,
-            }
-            form.save(**opts)
             return super().form_valid(form)
         else:
             form.errors['InvalidEmail'] = 'Введен некорректный email. Введите email, указанный при регистрации.'

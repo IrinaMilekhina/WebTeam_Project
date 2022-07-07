5dc9e9dcc2b32766b9d98fa038cbce1374f947c4
DanielPivnev <d.pivnev2003@gmail.com>
2022-07-07 12:37:52 +0200

    some front changes

--- ProductPlatform/static/assets/js/response.js
+++ ProductPlatform/static/assets/js/response.js
@@ -4,12 +4,16 @@
 let price_inp = document.querySelector("#price-suggestion");
 let description_textarea = document.querySelector("#description");
 let edit_btn = document.querySelector("#edit-btn");
+let cancel_btn = document.querySelector("#cancel-btn");
 let submit_btn = document.querySelector("#submit-btn");
+let save_btn = document.querySelector("#save-btn");
 const responses = document.querySelector("#res");
 
 
+console.log(price_inp, description_textarea)
+
 function make_readonly() {
-    if (price_inp.value !== '' && description_textarea.value !== '' && submit_btn.dataset.canceledresponse === "false") {
+    if (price_inp.value !== '' && description_textarea.value !== '') {
         price_inp.setAttribute("readonly", "readonly");
         price_inp.classList.add("read_only");
         description_textarea.setAttribute("readonly", "readonly");
@@ -17,6 +21,7 @@ function make_readonly() {
 
         submit_btn.style.display = "none";
         edit_btn.style.display = "block";
+        cancel_btn.style.display = "block";
     }
 }
 
@@ -25,6 +30,11 @@ function make_modifiable() {
     price_inp.classList.remove("read_only");
     description_textarea.removeAttribute("readonly");
     description_textarea.classList.remove("read_only");
+
+    edit_btn.style.display = "none";
+    save_btn.style.display = "block";
 }
+
+edit_btn.addEventListener("click", make_modifiable)
 
 make_readonly();
